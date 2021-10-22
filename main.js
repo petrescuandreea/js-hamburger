9933b32f7bd7413635a62e7ba31799fd1b0f6265
Andreea1696 <andreeapetrescu16@gmail.com>
2021-10-22 15:57:00 +0200

    close hamburger-menu

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const hamburger = document.querySelector("fas");
+const hamburger = document.querySelector(".fas");
 
 hamburger.addEventListener("click",
 
     function() {
         
-        document.querySelector("hamburger-menu").style.display="block";
+        document.querySelector(".hamburger-menu").style.display="block";
 
+    }
+
+)
+
+
+
+const times = document.querySelector(".close");
+
+times.addEventListener("click",
+
+    function() {
+        document.querySelector(".hamburger-menu").style.display="none";
     }
 
 )
